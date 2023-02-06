18cdd83eb00f32f98c36c1ab49b9a5d59132b8c1
sewakgautam <sewak.gautam58@gmail.com>
2023-02-06 15:14:18 +0545

    swager changes

--- src/room/dto/Canpublish.dto.ts
+++ src/room/dto/Canpublish.dto.ts
@@ -3,7 +3,7 @@ import { ApiProperty } from '@nestjs/swagger';
 export class canPublishPremissionDto {
   @ApiProperty({ type: 'string', example: 'nepal' })
   roomId: string;
-  @ApiProperty({ type: 'string', example: 'nepal' })
+  @ApiProperty({ type: 'string', example: 'token' })
   identity: string;
   @ApiProperty({ type: 'string', example: 'Sewak' })
   userId: string;
